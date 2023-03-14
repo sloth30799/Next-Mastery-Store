f1c194308a0d35e76849a9863315ca837747c530
Han Ye Htun <sloth30799@gmail.com>
2023-03-15 01:48:31 +0630

    sanity cli

--- sanity_ecommerce/sanity.cli.ts
+++ sanity_ecommerce/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '5jyu6gl3',
-    dataset: 'production'
-  }
+    projectId: 'your-project-id',
+    dataset: 'production',
+  },
 })
